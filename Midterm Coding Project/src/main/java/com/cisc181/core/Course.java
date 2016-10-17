0482e5fdbd10c274c54e903e1a08f81c3161d667
engoodman <Emma@Emma>
2016-10-16 22:22:10 -0400

    Midterm

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
@@ -0,0 +1,19 @@
+package com.cisc181.core;
+import java.util.UUID;
+//Course
+public class Course {
+	public void CourseID(){
+		UUID CourseID = UUID.randomUUID();
+		System.out.println("Course ID: " + CourseID);
+	}
+	public String CourseName(){
+		return CourseName();
+	}
+	public int GradePoints(){
+		return GradePoints();
+	}
+	public String Major(){
+		return Major();
+	}
+
+}
